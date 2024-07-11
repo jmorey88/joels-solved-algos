92792b281e2165eb05b9aa4e38b321fd30bdbc08
jmorey88 <joel.morey88@gmail.com>
2024-07-11 06:52:30 -0700

    added another ruby algo

--- ruby-algos.rb
+++ ruby-algos.rb
@@ -970,3 +970,41 @@ def merge(nums1, m, nums2, n)
     nums1.delete_if { |char| char == 0 }
     nums1.sort!
 end
+
+# 217. Contains Duplicate
+# Easy
+# Topics
+# Companies
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,1]
+# Output: true
+# Example 2:
+
+# Input: nums = [1,2,3,4]
+# Output: false
+# Example 3:
+
+# Input: nums = [1,1,1,3,3,4,3,2,4,2]
+# Output: true
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 105
+# -109 <= nums[i] <= 109
+
+def contains_duplicate(nums)
+    nums.each { |i| 
+        nums.shift()
+        if nums.include?(i)
+            return true
+        else
+            return false
+        end
+    }
+end
