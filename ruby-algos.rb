27afbb16469a10f2930fba0ecb9cb853f5205ad2
jmorey88 <joel.morey88@gmail.com>
2024-07-25 07:07:16 -0700

    added anther ruby algo

--- ruby-algos.rb
+++ ruby-algos.rb
@@ -1511,3 +1511,91 @@ def is_valid(s)
   # If the stack is empty, all opening brackets have been matched and closed properly
   stack.empty?
 end
+
+# 18. 4Sum
+# Medium
+# Topics
+# Companies
+# Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+# 0 <= a, b, c, d < n
+# a, b, c, and d are distinct.
+# nums[a] + nums[b] + nums[c] + nums[d] == target
+# You may return the answer in any order.
+
+# Example 1:
+
+# Input: nums = [1,0,-1,0,-2,2], target = 0
+# Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+# Example 2:
+
+# Input: nums = [2,2,2,2,2], target = 8
+# Output: [[2,2,2,2]]
+
+# Constraints:
+
+# 1 <= nums.length <= 200
+# -109 <= nums[i] <= 109
+# -109 <= target <= 109
+
+# solution 1
+
+def four_sum(nums, target)
+  answer = []
+  combos = nums.combination(4).to_a
+  combos.each do |combo|
+    combo_sum = 0
+    combo.each do |i|
+      combo_sum += i
+    end
+    answer.push(combo) if combo_sum == target && !answer.include?(combo)
+  end
+  answer
+end
+
+# solution 1 refactored
+
+def four_sum(nums, target)
+  answer = []
+  nums.combination(4).each do |combo|
+    answer.push(combo.sort) if combo.sum == target && !answer.include?(combo.sort)
+  end
+  answer
+end
+
+# solution 2
+
+def four_sum(nums, target)
+  nums.sort!
+  n = nums.size
+  answer = []
+
+  (0...n - 3).each do |i|
+    next if i > 0 && nums[i] == nums[i - 1]
+
+    (i + 1...n - 2).each do |j|
+      next if j > i + 1 && nums[j] == nums[j - 1]
+
+      left = j + 1
+      right = n - 1
+
+      while left < right
+        sum = nums[i] + nums[j] + nums[left] + nums[right]
+
+        if sum == target
+          answer << [nums[i], nums[j], nums[left], nums[right]]
+          left += 1
+          right -= 1
+          left += 1 while left < right && nums[left] == nums[left - 1]
+          right -= 1 while left < right && nums[right] == nums[right + 1]
+        elsif sum < target
+          left += 1
+        else
+          right -= 1
+        end
+      end
+    end
+  end
+
+  answer
+end
