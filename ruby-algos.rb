80fd38ae10d27bf8efcafdd6e30fe0c176be4a17
jmorey88 <joel.morey88@gmail.com>
2024-07-28 08:33:53 -0700

    added another ruby algo

--- ruby-algos.rb
+++ ruby-algos.rb
@@ -1677,3 +1677,58 @@ def three_sum(nums)
 
   answer
 end
+
+# 16. 3Sum Closest
+# Medium
+# Topics
+# Companies
+# Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+
+# Return the sum of the three integers.
+
+# You may assume that each input would have exactly one solution.
+
+# Example 1:
+
+# Input: nums = [-1,2,1,-4], target = 1
+# Output: 2
+# Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+# Example 2:
+
+# Input: nums = [0,0,0], target = 1
+# Output: 0
+# Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+
+# Constraints:
+
+# 3 <= nums.length <= 500
+# -1000 <= nums[i] <= 1000
+# -104 <= target <= 104
+
+# solution 1
+
+def three_sum_closest(nums, target)
+  nums.sort!
+  n = nums.length
+  answer = nums[0] + nums[1] + nums[2]
+
+  (0...n - 2).each do |i|
+    left = i + 1
+    right = n - 1
+
+    while left < right
+      sum = nums[i] + nums[left] + nums[right]
+
+      answer = sum if (target - sum).abs < (target - answer).abs
+
+      if sum == target
+        return sum
+      elsif sum < target
+        left += 1
+      else
+        right -= 1
+      end
+    end
+  end
+  answer
+end
