de18aea5333f210ff679b95783a3cf4b10b6fd39
jmorey88 <joel.morey88@gmail.com>
2024-07-29 08:41:05 -0700

    added another ruby algo

--- ruby-algos.rb
+++ ruby-algos.rb
@@ -1732,3 +1732,63 @@ def three_sum_closest(nums, target)
   end
   answer
 end
+
+# 17. Letter Combinations of a Phone Number
+# Medium
+# Topics
+# Companies
+# Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+# A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+# Example 1:
+
+# Input: digits = "23"
+# Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+# Example 2:
+
+# Input: digits = ""
+# Output: []
+# Example 3:
+
+# Input: digits = "2"
+# Output: ["a","b","c"]
+
+# Constraints:
+
+# 0 <= digits.length <= 4
+# digits[i] is a digit in the range ['2', '9'].
+
+# solution 1
+
+def letter_combinations(digits)
+  return [] if digits.empty?
+
+  combos = {
+    '2' => 'abc',
+    '3' => 'def',
+    '4' => 'ghi',
+    '5' => 'jkl',
+    '6' => 'mno',
+    '7' => 'pqrs',
+    '8' => 'tuv',
+    '9' => 'wxyz'
+  }
+
+  answer = []
+
+  def backtrack(combination, next_digits, combos, answer)
+    if next_digits.empty?
+      answer << combination
+    else
+      digit = next_digits[0]
+      letters = combos[digit]
+      letters.each_char do |letter|
+        backtrack(combination + letter, next_digits[1..-1], combos, answer)
+      end
+    end
+  end
+
+  backtrack('', digits, combos, answer)
+  answer
+end
