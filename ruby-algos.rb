472cc377ad6d4e052c4422d5127ebabd09d84c93
jmorey88 <joel.morey88@gmail.com>
2024-08-11 00:20:30 -0700

    added another ruby algo

--- ruby-algos.rb
+++ ruby-algos.rb
@@ -2259,3 +2259,73 @@ def find_words(words)
 
   answer
 end
+
+# 290. Word Pattern
+# Easy
+# Topics
+# Companies
+# Given a pattern and a string s, find if s follows the same pattern.
+
+# Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+
+# Example 1:
+
+# Input: pattern = "abba", s = "dog cat cat dog"
+# Output: true
+# Example 2:
+
+# Input: pattern = "abba", s = "dog cat cat fish"
+# Output: false
+# Example 3:
+
+# Input: pattern = "aaaa", s = "dog cat cat dog"
+# Output: false
+
+# Constraints:
+
+# 1 <= pattern.length <= 300
+# pattern contains only lower-case English letters.
+# 1 <= s.length <= 3000
+# s contains only lowercase English letters and spaces ' '.
+# s does not contain any leading or trailing spaces.
+# All the words in s are separated by a single space.
+
+# solution 1 has problems
+
+def word_pattern(pattern, s)
+  pattern = pattern.split('')
+  s = s.split(' ')
+
+  tracker = {}
+
+  return false if pattern.length != s.length
+
+  (0..pattern.length).each do |i|
+    if !tracker.include?(pattern[i]) && !tracker.include?(s[i])
+      tracker[pattern[i]] = s[i]
+    elsif tracker.include?(pattern[i]) && tracker[pattern[i]] != s[i]
+      return false
+    end
+  end
+  true
+end
+
+# solution 1 fixed
+
+def word_pattern(pattern, s)
+  pattern = pattern.split('')
+  s = s.split(' ')
+
+  tracker = {}
+
+  return false if pattern.length != s.length
+
+  (0..pattern.length).each do |i|
+    if !tracker.include?(pattern[i]) && !tracker.include?(s[i])
+      tracker[pattern[i]] = s[i]
+    elsif tracker.include?(pattern[i]) && tracker[pattern[i]] != s[i]
+      return false
+    end
+  end
+  true
+end
